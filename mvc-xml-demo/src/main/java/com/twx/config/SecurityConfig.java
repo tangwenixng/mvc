1d17312f94142dfe8a8ed93432bc465d000f611c
“唐文兴 <tangwenxing@nebulae.com.cn>
2017-11-04 23:15:06 +0800

    增加logback日志配置、spring security配置

---
+++ mvc-xml-demo/src/main/java/com/twx/config/SecurityConfig.java
@@ -0,0 +1,29 @@
+package com.twx.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * Created by twx on 2017/11/4.
+ */
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/list/**").access("hasRole('ADMIN')")
+                .and().formLogin();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("user").password("password").roles("USER").and()
+                .withUser("admin").password("admin").roles("USER","ADMIN");
+    }
+}
--- mvc-xml-demo/src/main/java/com/twx/controller/HomeController.java
+++ mvc-xml-demo/src/main/java/com/twx/controller/HomeController.java
@@ -1,5 +1,7 @@
 package com.twx.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,8 +16,14 @@ import java.util.List;
  */
 @Controller
 public class HomeController {
+    private final Logger logger = LoggerFactory.getLogger(HomeController.class);
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public String getHome(Model model) {
+        logger.trace("trace logging");
+        logger.debug("debug logging");
+        logger.info("info logging");
+        logger.warn("warning logging");
+//        logger.error("error logging", new RuntimeException("help"));
         model.addAttribute("serverTime", "11-02");
         return "home";
     }
